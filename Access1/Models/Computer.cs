9a2960682f649a36ce4bd9e34c277e4b6f1a4895
gunel773 <gunelmg@code.edu.az>
2023-11-12 03:59:00 +0400

    access mod learn code add

---
+++ Access1/Models/Computer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access1.Models
+{
+    internal class Computer
+    {
+        readonly string color = "red"; //class icinde set elemek olr ve construktorda
+        public string Model;
+        private int _ram;
+        protected int Memorage;
+        public int Ram
+        {
+            get
+            {
+                return _ram;
+            }
+            set
+            {
+                _ram = Ram;
+            }
+        }
+
+        public Computer(string model, int ram, int memorage, string color)
+        {
+            model = Model;
+            memorage = Memorage;
+            ram = _ram;
+            color = "green";
+        }
+
+
+        public void Info()
+        {
+            Console.WriteLine(Model,Memorage,_ram);
+        }
+            
+        
+         
+    }
+}
---
+++ Access1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Access1.Models;
+
+
+//Model publicdir. Hem constructorun hemde methodun icinde ,cagirmaq olur.
+//ram privatedir.
+//memorage protected dir.
+Computer comp = new Computer();
+comp.Model = "Honor";
+comp.Memorage = 256; //error  verir ,cunki protected instance de el catan deil
+comp._ram = 8; //error verir cunki private di
+
+//encapsulation
+comp.Ram = 16;
+Console.WriteLine(comp.Ram);
+
+//readonly fields
+comp.Color = "black";  // error verir cunki set elemk olmaz
+
+
